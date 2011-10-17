3e00e1d810ad4629ebf987ef57c7349f3c222987
aecker <aecker@d1c95485-6871-e240-9df0-63fa95a8ea69>
2011-10-17 01:49:35 +0000

    Minor fixes in HDF5 access
    git-svn-id: svn://at-lab.neusc.bcm.tmc.edu/libraries/hdf5matlab@4036 d1c95485-6871-e240-9df0-63fa95a8ea69

--- @H5Tools/openFamily.m
+++ @H5Tools/openFamily.m
@@ -5,10 +5,6 @@ if nargin < 2
 end
 
 fapl = H5P.create('H5P_FILE_ACCESS');
-if (strmatch(flags, 'H5F_ACC_RDWR'))
-    H5P.set_fapl_family(fapl, 2147483648, 'H5P_DEFAULT');
-else
-    H5P.set_fapl_family(fapl, 0, 'H5P_DEFAULT');
-end
+H5P.set_fapl_family(fapl, 0, 'H5P_DEFAULT');
 fp = H5F.open(fname, flags, fapl);
 H5P.close(fapl);
--- raw/@baseReaderElectrophysiology/getSampleIndex.m
+++ raw/@baseReaderElectrophysiology/getSampleIndex.m
@@ -2,7 +2,6 @@ function idx = getSampleIndex(br, t)
 % idx = getSampleIndex(br, t)
 % Returns the sample indices 'idx' for the vector of timestamps 't'.
 
-warning('check me')
 idx = round(1e-3 * (t - br.t0) * br.Fs) + 1;
 idx(idx < 1) = nan;
 idx(idx > br.nbSamples) = nan;

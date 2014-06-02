b4f51e41edda9b92236e2669e257cb4e4f01653d
Edgar Y. Walker <edgar.walker@gmail.com>
2014-06-02 01:40:48 -0500

    modified setPath

--- setPath.m
+++ setPath.m
@@ -1,6 +1,6 @@
 function setPath
-
-% Add subfolders for HDF5 access to the path
-addpath(fileparts(mfilename('fullpath')))
-addpath(fullfile(fileparts(mfilename('fullpath')), 'raw'))
-addpath(fullfile(fileparts(mfilename('fullpath')), 'spikes'))
+    % Add subfolders for HDF5 access to the path
+    addpath(fileparts(mfilename('fullpath')))
+    addpath(fullfile(fileparts(mfilename('fullpath')), 'raw'))
+    addpath(fullfile(fileparts(mfilename('fullpath')), 'spikes'))
+end

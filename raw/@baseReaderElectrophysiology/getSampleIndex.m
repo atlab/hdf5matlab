12babade6a1aefdf78fde6447a13c4be18886130
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2015-01-16 12:57:23 -0600

    Ensuring t0 is updated in getSampleIndex

--- raw/@baseReaderElectrophysiology/getSampleIndex.m
+++ raw/@baseReaderElectrophysiology/getSampleIndex.m
@@ -2,6 +2,7 @@ function idx = getSampleIndex(br, t)
 % idx = getSampleIndex(br, t)
 % Returns the sample indices 'idx' for the vector of timestamps 't'.
 
+assert(br.t0 > 0, 't0 has not been updated in this file!')
 idx = round(1e-3 * (t - br.t0) * br.Fs) + 1;
 idx(idx < 1) = nan;
 idx(idx > br.nbSamples) = nan;
